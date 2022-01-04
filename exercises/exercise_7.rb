d396636060a98a4d79436b457e403870740f144a
Hang <jsu6898@gmail.com>
2022-01-03 22:25:59 -0800

    finishes ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,38 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store, presence: true
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :carry_one_apparel
+  
+  def carry_one_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "store has to carry one")
+      errors.add(:womens_apparel, "store has to carry one")
+    end
+  end
+end
+
+puts "please type in your store name"
+print "> "
+store_name = gets.chomp
+@store = Store.create(name: store_name)
+
+if !@store.valid?
+  @store.errors.messages.each {|key, message| puts "Error: #{key}: #{message.first}"}
+end
+
+puts "please type in the employee's name"
+print "> "
+employee_name = gets.chomp
+@employee = Employee.create(first_name: employee_name)
+
+if !@employee.valid?
+  @employee.errors.messages.each {|key, message| puts "Error: #{key}: #{message.first}"}
+end
