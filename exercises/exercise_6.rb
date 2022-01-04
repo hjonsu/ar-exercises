f3ae66e0019f697592910a9ea4f9e905aa220d90
Hang <jsu6898@gmail.com>
2022-01-03 22:04:50 -0800

    finishes ex 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,14 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Jonathan", last_name: "Su", hourly_rate: 120)
+@store2.employees.create(first_name: "Cyn", last_name: "Cui", hourly_rate: 120)
